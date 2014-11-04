1562e7aa6fadab966f5c59361ad8fda53be9855a
Michał Wadas <michalwadas@gmail.com>
2014-11-04 17:54:40 +0100

    Changes from gh-pages

--- Najtingalo.js
+++ Najtingalo.js
@@ -26,7 +26,7 @@
         }
 
         function commented(string) {
-            return '\n/*' + string + '*/\n';
+            return string.trim() === '' ? '': '\n/*' + string + '*/\n';
         }
 
         function BF_Node() {
@@ -49,7 +49,7 @@
 
         BF_Init.prototype = createPrototype(BF_Init, BF_Node);
         BF_Init.prototype.toString = function() {
-            return 'var heap = [];\n var pointer = 0;\n' + commented(this.comments);
+            return '\n var pointer = 0; ' + commented(this.comments);
         };
         function BF_NullNode() {
 
@@ -71,9 +71,10 @@
 
         BF_Right.prototype = createPrototype(BF_Left, BF_Node);
         BF_Right.prototype.toString = function() {
-            return 'pointer++;' + this.getComments();
+            return 'pointer = (pointer + '+this.by+')|0; ' + this.getComments();
         };
-
+        BF_Right.prototype.by = 1;
+        
         function BF_Left() {
 
         }
@@ -81,9 +82,9 @@
 
         BF_Left.prototype = createPrototype(BF_Left, BF_Node);
         BF_Left.prototype.toString = function() {
-            return 'pointer--;' + this.getComments();
+            return 'pointer = (pointer -'+this.by+')|0; ' + this.getComments();
         };
-
+        BF_Left.prototype.by = 1;
         function BF_Inc() {
 
         }
@@ -91,9 +92,9 @@
 
         BF_Inc.prototype = createPrototype(BF_Inc, BF_Node);
         BF_Inc.prototype.toString = function() {
-            return 'heap[pointer|0] = (heap[pointer]|0)+1;' + this.getComments();
+            return 'heap[pointer|0] = (heap[pointer]|0)+'+this.by+';' + this.getComments();
         };
-
+        BF_Inc.prototype.by = 1;
         function BF_Dec() {
 
         }
@@ -101,9 +102,10 @@
 
         BF_Dec.prototype = createPrototype(BF_Dec, BF_Node);
         BF_Dec.prototype.toString = function() {
-            return 'heap[pointer|0] = (heap[pointer]|0)-1;' + this.getComments();
+            return 'heap[pointer|0] = (heap[pointer]|0)-'+this.by+';' + this.getComments();
         };
-
+        BF_Dec.prototype.by = 1;
+        
         function BF_Output() {
 
         }
@@ -133,6 +135,7 @@
         BF_LoopStart.prototype.toString = function() {
             return 'while(heap[pointer]) { ' + this.getComments();
         };
+        
         function BF_LoopEnd() {
 
         }
@@ -142,6 +145,15 @@
         BF_LoopEnd.prototype.toString = function() {
             return '}' + this.getComments();
         };
+
+        function BF_FullLoop() {
+
+        }
+        BF_FullLoop.prototype = createPrototype(BF_FullLoop, BF_Node);
+        BF_FullLoop.prototype.toString = function() {
+            return BF_LoopStart.prototype.toString.call(this)+this.instructions.join('\n')+BF_LoopEnd.prototype.toString.call(this);
+        };
+
 
         var tokens = {
             '+' : BF_Inc,
@@ -158,6 +170,7 @@
             var parsed = [new BF_Init()];
             string = [].slice.call(string);
             var character;
+            var reachedFirstMeaningful = false;
             if (Najtingalo.isValid(string)) {
                 for ( i = 0; i < string.length; i++) {
                     character = string[i];
@@ -172,9 +185,37 @@
             }
             return parsed;
         };
-        Najtingalo.toRunnable = function(string) {
+        
+        Najtingalo.optimise = function(tokensList, level) {
+            if (level === 0)
+                return tokensList;
+            var clonedTokens = tokensList.slice(0);
+            var newTokens;
+            if (level >= 1 ) {
+                var tempList = [];
+                newTokens = clonedTokens.map(function(el, i, arr) {
+                    var nextEl = arr[i+1] || new BF_NullNode();
+                    if (nextEl.constructor === el.constructor && el.by !== undefined) {
+                        nextEl.comments = el.comments+'\n'+nextEl.comments;
+                        nextEl.by += el.by;
+                        el.by = null;
+                    }
+                    return el;
+                }).filter(function(el) {
+                    return el.by !== null;
+                });
+            }
+            /* outline */
+            if (level >= 2) {
+                
+            }
+            return newTokens;
+        };
+        
+        Najtingalo.toRunnable = function(string, optimisations) {
             var tokens = Najtingalo.parseTokens(string);
-            return Function('print', 'getInput', tokens.join('\n'));
+            tokens = Najtingalo.optimise(tokens, optimisations);
+            return Function('heap', 'print', 'getInput', tokens.join('\n'));
         };
         Najtingalo.isValid = function validateBF(string) {
             var i1 = 0;
